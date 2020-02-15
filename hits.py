bb867539bf25c2206c897dc6a86c5a493f8fff72
veraivanova233 <veraivanova233@gmail.com>
2020-02-15 13:14:59 +0300

    Add files via upload
    №2

---
+++ hits.py
@@ -0,0 +1,21 @@
+f = open("/Users/veraivanova/Downloads/hits.txt", 'r')
+f1 = f.readlines()
+f.close()
+ips = dict()
+for line in f1:
+    ip = line[line.find("\t")+1]
+    i = line.find("\t")+2
+    while (line[i].isdigit() or line[i] == '.') and line:
+        ip += line[i]
+        i += 1
+    if ip not in ips.keys():
+        ips[ip] = 0
+    else:
+        ips[ip] += 1
+for i in range(5):
+    for key, val in ips.items():
+        if val == max(ips.values()):
+            print(key)
+            ips[key] = min(ips.values())
+            break
+print(ips)
